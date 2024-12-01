873692baf92c35292f92174bc2646a9e36c01b92
Lcocks <epx8hh@virginia.edu>
2024-12-01 18:57:26 -0500

    Passing final edits to the module before installing as package.

---
+++ Final_Project/__init__.py
@@ -0,0 +1,1 @@
+print("Welcome to the Die game module!","\n  Available classes include Die(), Game(), and Analyzer()")
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+#Setup for the DS5100 Final Project of a Die creation, Game, and Analyzer and its corresponding test file.
+
+setup(
+    name='Setup_for_DS5100_Final_Project',
+
+    version='0.1',
+
+    description='Package created for using and testing Final_Project.',
+
+    url='https://github.com/Lcocks/DS5100_Final_Project',
+
+    author='Lou Cocks',
+
+    author_email='epx8hh@virginia.edu',
+
+    license='MIT',
+
+    packages=find_packages()
+
+)
+
+
+
+
